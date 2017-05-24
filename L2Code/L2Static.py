1c40a11e5fc5a5cbd0edd912f2c0c23a5ebcd8ed
yatuhashi kei <keyansuiya@gmail.com>
2017-05-25 02:16:52 +0900

    fix L2Static

--- L2Code/L2Static.py
+++ L2Code/L2Static.py
@@ -17,31 +17,31 @@ class L2StaticEntry():
         parser = self.datapath.ofproto_parser
         match = parser.OFPMatch(eth_dst=mac)
         actions = [parser.OFPActionOutput(port)]
-        self.add_flow(self.datapath, 30000, match, actions, 0)
+        self.add_flow(30000, match, actions, 0)
 
     def flood_entry(self):  # L2 Flood
         parser = self.datapath.ofproto_parser
         match = parser.OFPMatch(eth_dst='ff:ff:ff:ff:ff:ff')
         actions = [parser.OFPActionOutput(ofproto_v1_3.OFPP_FLOOD)]
-        self.add_flow(self.datapath, 30000, match, actions, 0)
+        self.add_flow(30000, match, actions, 0)
 
     def nomatch_entry(self):  # L2 out
         parser = self.datapath.ofproto_parser
         match = parser.OFPMatch(eth_type=0x0800, ipv4_dst=(self.gateway_subnet_ip, self.gateway_subnet_mask))
         actions = [parser.OFPActionOutput(self.gateway_port)]
-        self.add_flow(self.datapath, 29999, match, actions, 0)
+        self.add_flow(29999, match, actions, 0)
 
-    def add_flow(self, datapath, priority, match, actions, idle_timeout):
+    def add_flow(self, priority, match, actions, idle_timeout):
         """
         datapath : Datapath
         priority : 最大65535 大きいほど優先される
         match : 来たパケットのマッチ条件
         actions : マッチしたパケットをどうするか
         """
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
+        ofproto = self.datapath.ofproto
+        parser = self.datapath.ofproto_parser
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
                                              actions)]
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+        mod = parser.OFPFlowMod(datapath=self.datapath, priority=priority,
                                 match=match, instructions=inst, idle_timeout=idle_timeout)
-        datapath.send_msg(mod)
+        self.datapath.send_msg(mod)
